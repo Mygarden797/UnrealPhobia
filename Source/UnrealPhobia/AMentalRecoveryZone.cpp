d1e89ede8963ba4a594a8c14c340116df0644c50
Lee <allblack797@naver.com>
2025-05-06 17:31:25 +0900

    20025-05-06

---
+++ Source/UnrealPhobia/AMentalRecoveryZone.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AMentalRecoveryZone.h"
+
+// Sets default values
+AAMentalRecoveryZone::AAMentalRecoveryZone()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AAMentalRecoveryZone::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AAMentalRecoveryZone::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
