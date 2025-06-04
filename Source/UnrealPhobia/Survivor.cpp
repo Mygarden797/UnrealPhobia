4ff77f74caec545c950256ccbec0a6a2a8f2d3b4
MPX2 <83400553+MPX2@users.noreply.github.com>
2025-06-04 21:41:23 +0900

    feat : 멘탈 기능 추가
    Survivor.cpp, Survivor.h 수정, 촛불방 구현

---
+++ Source/UnrealPhobia/Survivor.cpp
@@ -0,0 +1,325 @@
+#include "Survivor.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/Controller.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Blueprint/UserWidget.h"
+
+ASurvivor::ASurvivor(const FObjectInitializer& ObjectInitializer)
+{
+	// ĳ���� �浹 ũ��
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
+	
+	GetCharacterMovement()->bOrientRotationToMovement = false;					// ��Ʈ�ѷ� �������� ĳ���� ȸ��
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f); 
+	GetCharacterMovement()->bUseControllerDesiredRotation = true;
+	GetCharacterMovement()->MaxWalkSpeed = 400.f;
+	CurrentStamina = MaxStamina;
+	// UE_LOG(LogTemp, Error, TEXT("CurrentStamina, Init: %f"), CurrentStamina);
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(RootComponent);
+	SpringArm->TargetArmLength = 90.0f;
+	SpringArm->SocketOffset = FVector(10.f, 45.f, 0.f);
+	SpringArm->ProbeSize = 12.f;
+	// SpringArm->bUsePawnControlRotation = true; 
+
+	// SpringArm ������Ʈ ����
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(SpringArm, USpringArmComponent::SocketName); 
+	// FollowCamera->bUsePawnControlRotation = false; 
+
+	
+}
+
+void ASurvivor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	CurrentStamina = MaxStamina;
+	// UE_LOG(LogTemp, Error, TEXT("CurrentStamina, Init: %f"), CurrentStamina);
+
+	// Ű ���ε� ��������
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("Failed to Mapping Keys"));
+	}
+
+	// ���ؼ� ��������
+	if (IsLocallyControlled() && CrosshairWidgetClass)
+	{
+		CrosshairWidget =CreateWidget<UUserWidget>(GetWorld(), CrosshairWidgetClass);
+		if (CrosshairWidget) 
+		{
+			CrosshairWidget->AddToViewport();
+		}
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("Failed to load Crosshair!!"));
+	}
+
+// ��Ż����߰�(25.6.1)
+	CurrentMental = MaxMental;
+	// ��Ż �ڿ� ���� Ÿ�̸�
+	GetWorldTimerManager().SetTimer(
+		MentalDecayTimerHandle,
+		this,
+		&ASurvivor::DecreaseMental,
+		1.0f,  // �ʴ� 1ȸ
+		true
+	);
+// ��Ż����߰�(25.6.1)
+}
+
+void ASurvivor::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* EIC = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EIC->BindAction(MoveForwardAction, ETriggerEvent::Triggered, this, &ASurvivor::MoveForward);
+		EIC->BindAction(MoveRightAction, ETriggerEvent::Triggered, this, &ASurvivor::MoveRight);
+		EIC->BindAction(LookAction, ETriggerEvent::Triggered, this, &ASurvivor::Look);
+
+		
+		EIC->BindAction(SprintAction, ETriggerEvent::Started, this, &ASurvivor::Sprint);
+		EIC->BindAction(SprintAction, ETriggerEvent::Completed, this, & ASurvivor::Sprint);
+	}
+	else
+	{
+		// UE_LOG(LogTemp, Error, TEXT("Failed to Bind Input!!"));
+	}
+}
+
+void ASurvivor::MoveForward(const FInputActionValue& Value)
+{
+	const float AxisValue = Value.Get<float>();
+	if (Controller && AxisValue != 0.0f)
+	{
+		const FRotator YawRotation(0, Controller->GetControlRotation().Yaw, 0);
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(Direction, AxisValue);
+
+		// GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Move Forward"));
+	}
+	else 
+	{
+		// UE_LOG(LogTemp, Error, TEXT("No Controller or AxisValue is 0 in MoveForward!!"));
+	}
+}
+
+void ASurvivor::MoveRight(const FInputActionValue& Value)
+{
+	const float AxisValue = Value.Get<float>();
+
+	if (Controller && AxisValue != 0.0f)
+	{
+		const FRotator YawRotation(0, Controller->GetControlRotation().Yaw, 0);
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		AddMovementInput(Direction, AxisValue);
+
+		// GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Move Right"));
+	}
+	else
+	{
+		// UE_LOG(LogTemp, Error, TEXT("No Controller or AxisValue is 0 in MoveRight!!"));
+	}
+}
+
+void ASurvivor::Look(const FInputActionValue& Value)
+{
+	FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+	if (Controller != nullptr)
+	{
+		// add yaw and pitch input to controller
+		AddControllerYawInput(LookAxisVector.X);
+		AddControllerPitchInput(LookAxisVector.Y*-1);
+	}
+	else
+	{
+		// UE_LOG(LogTemp, Error, TEXT("No Controller in Look!!"));
+	}
+}
+
+void ASurvivor::Sprint(const FInputActionValue& Value)
+{
+	const bool DoSprint = Value.Get<bool>();			// LShift�� ������ ���� �� true
+
+	if (DoSprint && CurrentStamina > 0.f)
+	{
+		StartSprint();
+	}
+	else
+	{
+		StopSprint();
+	}
+}
+
+void ASurvivor::StartSprint()
+{
+	if (bIsSprinting || CurrentStamina <= 0.f)			// �޸��� ���̰ų� ���׹̳� ������ ����
+		return;
+
+	bIsSprinting = true;
+	GetCharacterMovement()->MaxWalkSpeed = 900.f;
+	bIsLossingStamina = true;
+
+	if (!GetWorldTimerManager().IsTimerActive(FStaminaLossHandle))
+	{
+			GetWorldTimerManager().ClearTimer(FStaminaLossHandle);
+			GetWorldTimerManager().SetTimer(
+			FStaminaLossHandle,
+			this,
+			&ASurvivor::LossStamina,
+			0.1f,
+			true
+		);
+	}
+	// UE_LOG(LogTemp, Display, TEXT("CurrentStamina, Start: %f"), CurrentStamina);
+}
+
+void ASurvivor::StopSprint()
+{
+	bIsSprinting = false;
+	GetCharacterMovement()->MaxWalkSpeed = 400.f;
+
+	bIsLossingStamina = false;
+	GetWorldTimerManager().ClearTimer(FStaminaLossHandle);
+
+	if (CurrentStamina < MaxStamina)
+	{
+		GetWorldTimerManager().SetTimer(
+			FStaminaRegenHandle, 
+			this, 
+			&ASurvivor::RegenStamina, 
+			1.0f, 
+			true);	
+	}
+	// UE_LOG(LogTemp, Display, TEXT("CurrentStamina, Stop: %f"), CurrentStamina);
+}
+
+void ASurvivor::LossStamina()
+{
+	if (!bIsLossingStamina || GetVelocity().SizeSquared() <= 0.0f)
+	{
+		// UE_LOG(LogTemp, Warning, TEXT("LossStamina: !bIsLossingStamina is False"));
+		return;
+	}
+
+	const float Delta = StaminaLossRate * 0.1f;
+	if (CurrentStamina <= Delta)
+	{
+		CurrentStamina = 0.0f;
+		// UE_LOG(LogTemp, Display, TEXT("LossStamina() - Sprint ����, Stamina: %.2f"), CurrentStamina);
+		StopSprint(); 
+	}
+
+	else
+	{
+		CurrentStamina -= Delta;
+		// UE_LOG(LogTemp, Display, TEXT("CurrentStamina, Loss: %f"), CurrentStamina);
+	}
+}
+
+void ASurvivor::RegenStamina()
+{
+	if (bIsSprinting || CurrentStamina >= MaxStamina)
+	{
+		GetWorldTimerManager().ClearTimer(FStaminaRegenHandle);
+		return;
+	}
+	else 
+	{
+		CurrentStamina += StaminaRegenRate * 0.1f;
+		// UE_LOG(LogTemp, Display, TEXT("CurrentStamina, Regen: %f"), CurrentStamina);
+		CurrentStamina = FMath::Min(CurrentStamina, MaxStamina);
+	}
+}
+
+// Mental(25.6.1.)
+
+void ASurvivor::IncreaseMental(float Amount)
+{
+	CurrentMental = FMath::Clamp(CurrentMental + Amount, 0.f, MaxMental);
+}
+
+void ASurvivor::DecreaseMental(float Amount)
+{
+	CurrentMental = FMath::Clamp(CurrentMental - Amount, 0.f, MaxMental);
+}
+
+// Mental
+void ASurvivor::DecreaseMental()
+{
+	if (CurrentMental > 0.f)
+	{
+		CurrentMental -= MentalDecayRate;
+		CurrentMental = FMath::Clamp(CurrentMental, 0.f, MaxMental);
+		UE_LOG(LogTemp, Log, TEXT("CurrentMental: %f"), CurrentMental);
+
+		if (CurrentMental <= 0.f)
+		{
+			GameOver();
+		}
+	}
+}
+
+void ASurvivor::GameOver()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Game Over! Mental is Zero."));
+}
+
+// Mental
+void ASurvivor::StartMentalRegen(float RegenAmountPerTick, float RegenInterval, float RegenTotalAmount)
+{
+	if (bIsInMentalRegenZone) return;
+
+	bIsInMentalRegenZone = true;
+	RegenStartMental = CurrentMental;
+	RegenTargetAmount = RegenTotalAmount;
+	MentalRegenPerTick = RegenAmountPerTick;
+
+	GetWorldTimerManager().SetTimer(
+		MentalRegenTimerHandle,
+		this,
+		&ASurvivor::RegenMental,
+		RegenInterval,
+		true
+	);
+}
+
+void ASurvivor::StopMentalRegen()
+{
+	bIsInMentalRegenZone = false;
+	GetWorldTimerManager().ClearTimer(MentalRegenTimerHandle);
+}
+
+void ASurvivor::RegenMental()
+{
+	if (CurrentMental >= MaxMental)
+	{
+		StopMentalRegen();
+		return;
+	}
+
+	if (CurrentMental - RegenStartMental >= RegenTargetAmount)
+	{
+		StopMentalRegen();
+		return;
+	}
+
+	CurrentMental = FMath::Clamp(CurrentMental + MentalRegenPerTick, 0.f, MaxMental);
+}
---
+++ Source/UnrealPhobia/Survivor.h
@@ -0,0 +1,139 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "InputActionValue.h"
+#include "Survivor.generated.h" // ����
+
+class USpringArmComponent;
+class UCameraComponent;
+class UInputMappingContext;
+class UInputAction;
+
+/*
+	- Name				: ASurvivor
+	- Description		: Player Character
+	- Date					: 2022/05/26, Hangyeol
+	- Fixed					: ������Ʈ �߿� ���׹̳ʰ� ȸ���Ǵ� ����
+*/
+
+UCLASS(Blueprintable)
+class UNREALPHOBIA_API ASurvivor : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ASurvivor(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(EditDefaultsOnly, Category = "UI")
+	TSubclassOf<class UUserWidget> CrosshairWidgetClass;
+	UPROPERTY()
+	class UUserWidget* CrosshairWidget;
+
+	// ĳ���� �յ� �̵�, 1D Vector
+	void MoveForward(const FInputActionValue& Value);
+	// ĳ���� �¿� �̵�, 1D Vector
+	void MoveRight(const FInputActionValue& Value);
+	// ī�޶� �̵�, 2D Vector
+	void Look(const FInputActionValue& Value);
+
+	// Sprint Handler 
+	void Sprint(const FInputActionValue& Value);
+	// LShift�� ������ ���� �� �޸���.
+	void StartSprint();
+	// LShift�� ������ ���� �� �޸��⸦ �����.
+	void StopSprint();
+
+	// ĳ���Ͱ� �޸��� �� ���׹̳ʰ� �����Ѵ�.
+	void LossStamina();
+	// ĳ���Ͱ� �޸��� ������ ���׹̳ʸ� ȸ���Ѵ�.
+	void RegenStamina();
+
+
+// ��Ż ��� �߰�(25.6.1)
+
+	void IncreaseMental(float Amount);
+	void DecreaseMental(float Amount); // �ܺ� �ǰݿ�
+
+	// ȸ�� ����
+	void StartMentalRegen(float RegenAmountPerTick, float RegenInterval, float RegenTotalAmount);
+	void StopMentalRegen();
+
+// ��Ż ��� �߰�(25.6.1)
+
+protected:
+	virtual void BeginPlay() override;
+
+	float CameraRotationSpeed = 50.f;
+
+	// Enhanced Input, IMC Default
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	TObjectPtr<UInputMappingContext> DefaultMappingContext;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	TObjectPtr<UInputAction> MoveForwardAction;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	TObjectPtr<UInputAction> MoveRightAction;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	TObjectPtr<UInputAction> LookAction;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	TObjectPtr<UInputAction> SprintAction;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera");
+	TObjectPtr<USpringArmComponent> SpringArm;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+	TObjectPtr<UCameraComponent> FollowCamera;
+
+	FRotator TargetCameraRotation;
+
+	// Mental 추가
+	void GameOver();
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Stamina")
+	float MaxStamina = 100.f;
+	UPROPERTY(VisibleAnywhere, Category = "Stamina")
+	float CurrentStamina = 100.f;
+	UPROPERTY(EditDefaultsOnly, Category = "Stamina")
+	float StaminaLossRate = 20.f;
+	UPROPERTY(EditDefaultsOnly, Category = "Stamina")
+	float StaminaRegenRate = 10.f;
+	UPROPERTY(EditDefaultsOnly, Category = "Stamina")
+
+	FTimerHandle FStaminaLossHandle;
+	FTimerHandle FStaminaRegenHandle;
+	bool bIsLossingStamina = false;
+	bool bIsSprinting = false;
+
+
+// ��Ż ��� �߰�(25.6.1)
+	UPROPERTY(EditDefaultsOnly, Category = "Mental")
+	float MaxMental = 200.f; // �ִ� ��Ż ����
+
+	UPROPERTY(VisibleAnywhere, Category = "Mental")
+	float CurrentMental = 200.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Mental")
+	float MentalDecayRate = 1.f; // �ʴ� 1 ����
+
+	FTimerHandle MentalDecayTimerHandle;
+
+	// ��Ż ȸ����
+	FTimerHandle MentalRegenTimerHandle;
+	bool bIsInMentalRegenZone = false;
+
+	float RegenStartMental = 0.f;
+	float RegenTargetAmount = 0.f;
+	float MentalRegenPerTick = 0.f;
+
+	void DecreaseMental(); // �ڿ� ���ҿ�
+	void RegenMental();    // ȸ�� Ÿ�̸ӿ�
+
+// ��Ż ��� �߰�(25.6.1)
+};
