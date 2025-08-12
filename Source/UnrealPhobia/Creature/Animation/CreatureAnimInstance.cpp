031d61a220bb0679745a4a6c7f8ea6b467d83edc
NerNoon <66264660+liam8014@users.noreply.github.com>
2025-08-13 04:18:08 +0900

    [feat] 라이팅 수정 및 크리쳐 카메라 전환 연출 추가
    - 크리쳐 카메라 전환 시 효과음 추가
    - 라이팅 방식 및 손전등 라이팅 수정
    - 카메라 전환시 FOV 변환 연출 추가

--- Source/UnrealPhobia/Creature/Animation/CreatureAnimInstance.cpp
+++ Source/UnrealPhobia/Creature/Animation/CreatureAnimInstance.cpp
@@ -1,99 +1,92 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Creature/Animation/CreatureAnimInstance.h"
 #include "Creature/CreatureController.h"
 #include "Creature/CreatureBase.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "GameFramework/Pawn.h"
 
-
 void UCreatureAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
 {
     Super::NativeUpdateAnimation(DeltaSeconds);
-    if(!Owner)
+    if (!Owner)
     {
         Owner = TryGetPawnOwner();
     }
-    
 
-    ACreatureBase* Creature = Cast<ACreatureBase>(Owner);
-    if(!Creature)
+    ACreatureBase *Creature = Cast<ACreatureBase>(Owner);
+    if (!Creature)
     {
         return;
     }
 
-    //현재 폰의 속도 가져오기
+    // 현재 폰의 속도 가져오기
     Speed = Creature->GetVelocity().Size();
-    
-    //현재 폰의 상태 가져오기
+
+    // 현재 폰의 상태 가져오기
     CurrentCreatureState = Creature->GetState();
-
-    
-
 }
 
 void UCreatureAnimInstance::PlayAttackMontage()
 {
-    if(!GetAttackMontage())
+    if (!GetAttackMontage())
     {
         UE_LOG(LogTemp, Display, TEXT("AttackMontage is nullptr"));
     }
-    
-    if(!Montage_IsPlaying(GetAttackMontage()))
+
+    if (!Montage_IsPlaying(GetAttackMontage()))
     {
         UE_LOG(LogTemp, Display, TEXT("AttackMontage is Activate"));
-        Montage_Play(GetAttackMontage());
+        Montage_Play(GetAttackMontage(), 2.0f);
     }
 }
 
 void UCreatureAnimInstance::PlayCommunicateMontage()
 {
-    if(!GetCommunicateMontage())
+    if (!GetCommunicateMontage())
     {
         UE_LOG(LogTemp, Display, TEXT("AttackMontage is nullptr"));
     }
-    
-    if(!Montage_IsPlaying(GetCommunicateMontage()))
+
+    if (!Montage_IsPlaying(GetCommunicateMontage()))
     {
         UE_LOG(LogTemp, Display, TEXT("AttackMontage is Activate"));
         Montage_Play(GetCommunicateMontage());
     }
 }
 
-class UBlendSpace* UCreatureAnimInstance::GetMovementBlendspace() const
+class UBlendSpace *UCreatureAnimInstance::GetMovementBlendspace() const
 {
-    RETURN_IF_NULL2(Owner,nullptr)
-    ACreatureBase* Creature = Cast<ACreatureBase>(Owner);
-    RETURN_IF_NULL2(Creature,nullptr)
+    RETURN_IF_NULL2(Owner, nullptr)
+    ACreatureBase *Creature = Cast<ACreatureBase>(Owner);
+    RETURN_IF_NULL2(Creature, nullptr)
 
-    UCreatureAnimationDataAsset* CreatureAnimationData = Creature->GetCreatureAnimationData();
-    RETURN_IF_NULL2(CreatureAnimationData,nullptr)
+    UCreatureAnimationDataAsset *CreatureAnimationData = Creature->GetCreatureAnimationData();
+    RETURN_IF_NULL2(CreatureAnimationData, nullptr)
 
     return CreatureAnimationData->MovementBlendspace;
-
 }
 
-class UAnimMontage* UCreatureAnimInstance::GetAttackMontage() const
+class UAnimMontage *UCreatureAnimInstance::GetAttackMontage() const
 {
-    RETURN_IF_NULL2(Owner,nullptr)
-    ACreatureBase* Creature = Cast<ACreatureBase>(Owner);
-    RETURN_IF_NULL2(Creature,nullptr)
+    RETURN_IF_NULL2(Owner, nullptr)
+    ACreatureBase *Creature = Cast<ACreatureBase>(Owner);
+    RETURN_IF_NULL2(Creature, nullptr)
 
-    UCreatureAnimationDataAsset* CreatureAnimationData = Creature->GetCreatureAnimationData();
-    RETURN_IF_NULL2(CreatureAnimationData,nullptr)
+    UCreatureAnimationDataAsset *CreatureAnimationData = Creature->GetCreatureAnimationData();
+    RETURN_IF_NULL2(CreatureAnimationData, nullptr)
 
     return CreatureAnimationData->AttackMontage;
 }
 
-class UAnimMontage* UCreatureAnimInstance::GetCommunicateMontage() const
+class UAnimMontage *UCreatureAnimInstance::GetCommunicateMontage() const
 {
-    RETURN_IF_NULL2(Owner,nullptr)
-    ACreatureBase* Creature = Cast<ACreatureBase>(Owner);
-    RETURN_IF_NULL2(Creature,nullptr)
+    RETURN_IF_NULL2(Owner, nullptr)
+    ACreatureBase *Creature = Cast<ACreatureBase>(Owner);
+    RETURN_IF_NULL2(Creature, nullptr)
 
-    UCreatureAnimationDataAsset* CreatureAnimationData = Creature->GetCreatureAnimationData();
-    RETURN_IF_NULL2(CreatureAnimationData,nullptr)
+    UCreatureAnimationDataAsset *CreatureAnimationData = Creature->GetCreatureAnimationData();
+    RETURN_IF_NULL2(CreatureAnimationData, nullptr)
 
     return CreatureAnimationData->CommunicateMontage;
 }
