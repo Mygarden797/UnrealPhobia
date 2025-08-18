2a9aefbd640a57bcb2bb4331da3444367a1bf7d6
GyeolCrash <sclassrunner333@gmail.com>
2025-08-18 13:56:07 +0900

    Feat: Implement PropagationZone and Propagation Manager
    - Implement PropagationZoneActor based AActor
    - Implement PropagationManager based UGameInstanceSubsystem

---
+++ Source/UnrealPhobia/Managers/SoundPropagationManager.h
@@ -0,0 +1,47 @@
+﻿// Copyright © 2025 Unrealphobia. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "SoundPropagationManager.generated.h"
+
+/**
+ * 
+ */
+
+class PropagationZoneActor;
+
+UCLASS()
+class UNREALPHOBIA_API USoundPropagationManager : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+    void RegisterZone(APropagationZoneActor* Zone);
+    void UnregisterZone(APropagationZoneActor* Zone);
+
+    void NotifyActorEntered(APropagationZoneActor* Zone, AActor* Actor);
+    void NotifyActorExited(APropagationZoneActor* Zone, AActor* Actor);
+
+    UFUNCTION(BlueprintCallable, Category = "Sound Propagation")
+    APropagationZoneActor* GetZoneOfActor(AActor* Actor) const;
+
+    UFUNCTION(BlueprintCallable, Category = "Sound Propagation")
+    void GetActorsInSameZone(AActor* Context, TArray<AActor*>& OutActors) const;
+
+    UFUNCTION(BlueprintCallable)
+    void PropagateSoundFrom(AActor* Emitter, float BaseLoudness);
+
+protected:
+
+
+private:
+    UPROPERTY()
+    TSet<APropagationZoneActor*> RegisteredZones;
+
+    UPROPERTY()
+    TMap<AActor*, APropagationZoneActor*> ActorToZone;
+
+    TMap<APropagationZoneActor*, TSet<AActor*>> ZoneActors;
+};
