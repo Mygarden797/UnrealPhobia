be7baf5db4a67beebcbd53f621fdf71c852194b7
GyeolCrash <sclassrunner333@gmail.com>
2025-08-24 23:24:23 +0900

    Chore: Delete Unrealphobia Project

--- Source/UnrealPhobia/Trigger/TriggerSpawnPoint.cpp
+++
@@ -1,7 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "TriggerSpawnPoint.h"
-ATriggerSpawnPoint::ATriggerSpawnPoint()
-{
-    SpawnerId = 1;
-}
--- Source/UnrealPhobia/UnrealPhobia.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "UnrealPhobia.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealPhobia, "UnrealPhobia" );
- 
