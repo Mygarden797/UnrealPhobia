66fa3fb29c94ea02e99b05337c3e06b960352e9c
GyeolCrash <sclassrunner333@gmail.com>
2025-08-24 23:34:25 +0900

    Fix: Update AudioAsset
    - Fix AudioAsset.cpp
    - Change DA_AudioAsset from Blueprint Class type to Data Asset type

--- Source/Paranoid/Assets/AudioAssets.cpp
+++ Source/Paranoid/Assets/AudioAssets.cpp
@@ -18,30 +18,17 @@ UAudioAssets* UAudioAssets::LoadAudioAssets()
         if (!AudioAssetPath.IsEmpty())
         {
             FSoftObjectPath AudioDataPath(AudioAssetPath);
-
-            // 먼저 DataAsset으로 직접 로드 시도
             CachedAudioAssets = Cast<UAudioAssets>(AudioDataPath.TryLoad());
-
-            // 실패시 Blueprint로 로드
-            // 현 프로젝트에서는 블루프린트로 동작할 예정
-            if (!CachedAudioAssets)
-            {
-                UBlueprint* LoadedBlueprint = Cast<UBlueprint>(AudioDataPath.TryLoad());
-                if (LoadedBlueprint && LoadedBlueprint->GeneratedClass)
-                {
-                    CachedAudioAssets = Cast<UAudioAssets>(LoadedBlueprint->GeneratedClass->GetDefaultObject());
-                }
-            }
         }
     }
 
     if (!CachedAudioAssets)
     {
-        UE_LOG(LogTemp, Error, 
-            TEXT("AudioAssets::LoadAudioAssets(): Failed to load AudioAssets from path: %s"), 
+        UE_LOG(LogTemp, Error,
+            TEXT("AudioAssets::LoadAudioAssets(): Failed to load AudioAssets from path: %s"),
             *AudioAssetPath);
+        return nullptr;
     }
 
     return CachedAudioAssets;
 }
-
